12f46a40d73680ab42967cdc70a14feee9bc5b83
zzh1206 <135524696+zzh1206@users.noreply.github.com>
2023-10-22 14:26:20 +0800

    Update 正方体2.cpp

--- 正方体2.cpp
+++ 正方体2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout<<"                * * * * * * * *"<<endl<<"               *             **"<<endl<<"              *             * *"<<endl<<"             *             *  *"<<endl<<"            *             *   *"<<endl<<"           * * * * * * * *    *"<<endl<<"           *             *    *"<<endl<<"           *             *   * "<<endl<<"           *             *  *  "<<endl<<"           *             * *   "<<endl<<"           *             **    "<<endl<<"           * * * * * * * *     "<<endl<<"            这就是我的创意图形——正方体。"<<"源代码请看“”"<<endl; 
+	cout<<"                * * * * * * * *"<<endl<<"               *             **"<<endl<<"              *             * *"<<endl<<"             *             *  *"<<endl<<"            *             *   *"<<endl<<"           * * * * * * * *    *"<<endl<<"           *             *    *"<<endl<<"           *             *   * "<<endl<<"           *             *  *  "<<endl<<"           *             * *   "<<endl<<"           *             **    "<<endl<<"           * * * * * * * *     "<<endl<<"            这就是我的创意图形——正方体。"<<"源代码请看“https://github.com/zzh1206/c_homework/edit/main/%E6%AD%A3%E6%96%B9%E4%BD%932.cpp”"<<endl; 
 	return 0;
 } 
